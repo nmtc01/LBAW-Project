871f622158bbc4a4aa9ec6374b98700f2683a949
Eduardo Macedo <eduardojcmacedo@gmail.com>
2020-03-04 20:37:36 +0000

    moderator progress

---
+++ template/moderator.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+    <style type="text/css">
+        
+        body {
+            background: #eee;
+            background-image: url("resources/wallpaper2.png");
+        }
+
+        .wrapper {
+            margin: 70px;
+        }
+
+        .form-signin {
+            max-width: 380px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 15px 40px 50px;
+            border: 1px solid #e5e5e5;
+            border-radius: 10px;
+        }
+
+        .form-signin input[type="text"],
+        .form-signin input[type="password"] {
+            margin-bottom: 20px;
+        }
+
+        .form-signin #api {
+            margin-bottom: 40px;
+        }
+
+        .form-signin .form-signin-heading {
+            margin-bottom: 50px;
+        }
+
+        .jumbotron {
+            background: #eee;
+            text-align: center;
+        }
+
+        #register {
+            margin-top: 10px;
+        }
+
+        #logo {
+            margin-top: 50px;
+            text-align: center;
+        }
+
+        h1{
+            margin-bottom: 2rem;
+        }
+        
+        h1 > span {
+            margin-left: 2.3rem;
+            color: white;
+            font-size: 30px;
+            border-bottom: 1px solid black;
+            padding-bottom: 5px;
+            border-color: #6545c9;
+        }
+
+        /* scrolltable rules */
+        table  { 
+            margin-top:  20px; display: inline-block; overflow: auto; 
+        }
+        th div { 
+            margin-top: -20px; position: absolute; 
+        }
+
+        /* design */
+        table { 
+            padding-left: 3rem;
+            border-collapse: collapse; 
+        }
+        tr { 
+            background: #FFF; 
+        }
+        th {
+            color: white;
+        }
+        .header{
+        }
+
+    </style>
+</head>
+
+<body>
+    <script defer src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+    <header>    
+
+        
+        <nav class="navbar navbar-light" style="background-color: #6545c9">
+            <a class="navbar-brand" ></a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit" style="background-color:	white; color: #6545c9">Search</button>
+            </form>
+        </nav>
+        
+
+
+    </header>
+
+    <h1><span>Reports</span></h1>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <table style="height: 150px; width: 20rem">
+  <tr> <th><div>first</div> <th>
+  <tr> <td>foo <td>
+  <tr> <td>foo foo  foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+</table>
+    </div>
+    <div class="col">
+      <table style="height: 150px; width: 20rem">
+  <tr> <th><div>first</div> <th>
+  <tr> <td>foo <td>
+  <tr> <td>foo fo foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  
+</table>
+    </div>
+    <div class="col">
+      <table style="height: 150px; width: 20rem">
+  <tr> <th><div>first</div> <th>
+  <tr> <td>foo <td>
+  <tr> <td>foo fooo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+  <tr> <td>foo <td>
+</table>
+    </div>
+  </div>
+  
+</div>
+
+
+
+</body>
+
+</html>
