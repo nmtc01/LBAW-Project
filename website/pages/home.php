301b611816e4dfe71f155cd328fe40f2754870c6
antoniopedrodantas <pedro.dantascv@gmail.com>
2020-03-05 15:38:24 +0000

    home page under construction

---
+++ website/pages/home.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/6d90d25abc.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../css/home.css">
+
+    <style type="text/css">
+    </style>
+</head>
+
+<body>
+    <script defer src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+    <header>
+
+
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #6545c9">
+            <a href="#" class="navbar-brand" style="color: white;">Answerly</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="color: white;">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <button class="btn my-2 my-sm-0" type="submit" style="background-color:	white; color:#6545c9">Ask
+                            Something</button>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li>
+                        <a class="nav-link" href="#" style="color: white">
+                            <i class="fas fa-user fa-lg"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="#" style="color: white">
+                            <i class="fas fa-bell fa-lg"></i>
+                        </a>
+                    </li>
+                </ul>
+
+
+
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn my-2 my-sm-0" type="submit" style="background-color:	white; color:#6545c9">Search</button>
+                </form>
+            </div>
+
+
+
+        </nav>
+
+    </header>
+
+    <button class="input-button">
+        What is your question?
+    </button>
+
+    </button>
+
+
+    <div class="wrapper">
+        <div class="media">
+            <img src="resources/adam_sandler.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic" width="64px"
+                height="64px">
+            <div class="media-body">
+                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
+                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
+                research for my next movie and I wnat it to be 100% cientifically correct...
+            </div>
+
+        </div>
+
+        <br>
+
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-up fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-down fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="fas fa-reply fa-lg"></i>
+        </a>
+
+
+    </div>
+
+    <div class="wrapper">
+        <div class="media">
+            <img src="resources/bob_iger.jpeg" class="align-self-start mr-3" alt="adam_sandler profile pic" width="64px"
+                height="64px">
+            <div class="media-body">
+                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
+                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
+                research for my next movie and I wnat it to be 100% cientifically correct...
+            </div>
+
+        </div>
+
+        <br>
+
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-up fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-down fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="fas fa-reply fa-lg"></i>
+        </a>
+    </div>
+
+    <div class="wrapper">
+        <div class="media">
+            <img src="resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic" width="64px"
+                height="64px">
+            <div class="media-body">
+                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
+                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
+                research for my next movie and I wnat it to be 100% cientifically correct...
+            </div>
+
+        </div>
+
+        <br>
+
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-up fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-down fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="fas fa-reply fa-lg"></i>
+        </a>
+    </div>
+
+    <div class="wrapper">
+        <div class="media">
+            <img src="resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic" width="64px"
+                height="64px">
+            <div class="media-body">
+                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
+                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
+                research for my next movie and I wnat it to be 100% cientifically correct...
+            </div>
+
+        </div>
+
+        <br>
+
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-up fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-down fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="fas fa-reply fa-lg"></i>
+        </a>
+    </div>
+
+    <div class="wrapper">
+        <div class="media">
+            <img src="resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic" width="64px"
+                height="64px">
+            <div class="media-body">
+                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
+                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
+                research for my next movie and I wnat it to be 100% cientifically correct...
+            </div>
+
+        </div>
+
+        <br>
+
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-up fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="far fa-thumbs-down fa-lg"></i>
+        </a>
+        <a class="icon" href="#" style="color: #6545c9">
+            <i class="fas fa-reply fa-lg"></i>
+        </a>
+    </div>
+
+
+    <div class="btn-group-vertical">
+        <span class="label success">Success</span>
+        <span class="label info">Info</span>
+        <span class="label warning">Warning</span>
+        <span class="label danger">Danger</span>
+        <span class="label other">Other</span>
+    </div>
+
+</body>
+
+</html>
