a20ceb7166c64084bd69c47c262afa9f2b949586
Nuno <up201706162@fe.up.pt>
2020-03-06 22:17:39 +0000

    Refactoring with templates

--- website/pages/home.php
+++ website/pages/home.php
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html>
+<?php
+    include_once "../template/tpl_common.php";
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/6d90d25abc.js" crossorigin="anonymous"></script>
+    draw_main_document();
+?>
 
     <link rel="stylesheet" href="../css/home.css">
-    <link rel="stylesheet" href="../css/layout.css">
 
 </head>
 
 <body>
-    <script defer src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light navbar-default fixed-top">
             <div class="container">
@@ -97,105 +86,6 @@
     </div>
     <? }
     ?>
-
-    <!--
-    <div class="wrapper">
-        <div class="media">
-            <img src="../resources/bob_iger.jpeg" class="align-self-start mr-3" alt="adam_sandler profile pic">
-            <div class="media-body">
-                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
-                Lorem ipsum dolor sit amet, nullam posuere urna duis id maecenas feugiat, dictum ante proin praesent at, dolor laoreet arcu, gravida pede gravida hymenaeos. Diam nunc leo tristique sed vel augue, non sem. Velit molestie diam justo turpis molestie justo, justo et. Aenean integer luctus aliquam massa, nunc aliquam varius justo, aliquam non ipsum elit tellus maecenas, consequat consectetuer nascetur sem, vel tellus. Eu purus, viverra sociosqu faucibus cras integer facilisis aenean, libero voluptatem dolor non class vivamus nulla, elit turpis auctor gravida, vel tempor.
-            </div>
-
-        </div>
-
-        <br>
-
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-up fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-down fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="fas fa-reply fa-lg"></i>
-        </a>
-    </div>
-
-    <div class="wrapper">
-        <div class="media">
-            <img src="../resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic">
-            <div class="media-body">
-                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
-                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
-                research for my next movie and I wnat it to be 100% cientifically correct...
-            </div>
-
-        </div>
-
-        <br>
-
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-up fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-down fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="fas fa-reply fa-lg"></i>
-        </a>
-    </div>
-
-    <div class="wrapper">
-        <div class="media">
-            <img src="../resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic">
-            <div class="media-body">
-                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
-                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
-                research for my next movie and I wnat it to be 100% cientifically correct...
-            </div>
-
-        </div>
-
-        <br>
-
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-up fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-down fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="fas fa-reply fa-lg"></i>
-        </a>
-    </div>
-
-    <div class="wrapper">
-        <div class="media">
-            <img src="../resources/will-ferrel.jpg" class="align-self-start mr-3" alt="adam_sandler profile pic">
-            <div class="media-body">
-                <h5 class="mt-0">Adam Sandler's cool experiment</h5>
-                Hey guys! Can anyone tell me if mixing coke with mentos creates an explosive reaction? I'm doing
-                research for my next movie and I wnat it to be 100% cientifically correct...
-            </div>
-
-        </div>
-
-        <br>
-
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-up fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="far fa-thumbs-down fa-lg"></i>
-        </a>
-        <a class="icon" href="#">
-            <i class="fas fa-reply fa-lg"></i>
-        </a>
-    </div>
-    -->
-
-
     <div class="sidenav">
         <div class="elements">
             <a href="#">Sports</a>
@@ -206,8 +96,6 @@
         </div>
     </div>
 
-    
-
-</body>
-
-</html>
+<?php
+    draw_footer();
+?>
--- website/pages/search.php
+++ website/pages/search.php
@@ -1,23 +1,14 @@
-<!DOCTYPE html>
-<html>
+<?php
+    include_once "../template/tpl_common.php";
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-        crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/layout.css">
+    draw_main_document();
+?>
+
     <link rel="stylesheet" href="../css/list_format.css">
     <link rel="stylesheet" href="../css/search.css">
 </head>
 
 <body>
-    <script defer src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
     <header>
         <div class="fixed-top">
             <nav class="navbar navbar-expand-lg navbar-light navbar-default">
@@ -100,86 +91,6 @@
             </li>
         </ul>
     </nav>
-
-    <footer class="page-footer font-small indigo">
-        <!-- Footer Links -->
-        <div class="container text-center text-md-left">
-        <!-- Grid row -->
-            <div class="row">
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto">
-                    <!-- Links -->
-                    <h3 class="font-weight-bold text-uppercase mt-3 mb-4">Labels</h3>
-                    <ul class="list-unstyled">
-                        <li>
-                        <a href="#!">Access my profile</a>
-                        </li>
-                        <li>
-                        <a href="#!">Add a question</a>
-                        </li>
-                        <li>
-                        <a href="#!">Get back home</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Grid column -->
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto">
-                <!-- Links -->
-                    <h3 class="font-weight-bold text-uppercase mt-3 mb-4">Labels</h3>
-                    <ul class="list-unstyled">
-                        <li>
-                        <a href="#!">#Sports</a>
-                        </li>
-                        <li>
-                        <a href="#!">#Astronomy</a>
-                        </li>
-                        <li>
-                        <a href="#!">#Web_development</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Grid column -->
-                <!-- Grid column -->
-                <div class="col-md-3 mx-auto">
-                    <!-- Links -->
-                    <h3 class="font-weight-bold text-uppercase mt-3 mb-4">Labels</h3>
-                    <ul class="list-unstyled">
-                        <li>
-                        <a href="#!">Physics</a>
-                        </li>
-                        <li>
-                        <a href="#!">Anatomy</a>
-                        </li>
-                        <li>
-                        <a href="#!">Comics</a>
-                        </li>
-                    </ul>
-                </div>
-                    <!-- Grid column -->
-                    <div class="col-md-3 mx-auto">
-                    <!-- Links -->
-                    <h3 class="font-weight-bold text-uppercase mt-3 mb-4">About</h3>
-                    <ul class="list-unstyled">
-                        <li>
-                        <a href="#!">About us</a>
-                        </li>
-                        <li>
-                        <a href="#!">Contacts</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Grid column -->
-            </div>
-        <!-- Grid row -->
-        </div>
-        <!-- Footer Links -->
-        <!-- Copyright -->
-        <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="index.php"> Answerly</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
-</body>
-
-</html>
+<?php
+    draw_footer();
+?>
