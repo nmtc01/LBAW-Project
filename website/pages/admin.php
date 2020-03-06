e34e7ca2fce0f45ad25bbc50fc1ae25083573c75
Eduardo Macedo <eduardojcmacedo@gmail.com>
2020-03-06 22:34:29 +0000

    grr

---
+++ website/pages/admin.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/modmin.css">
+
+</head>
+
+<body>
+    <script defer src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+    <header>    
+
+        
+        <nav class="navbar navbar-light" style="background-color: #6545c9">
+            <a class="navbar-brand" ></a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit" style="background-color:	white; color: #6545c9">Search</button>
+            </form>
+        </nav>
+        
+
+
+    </header>
+
+    <h1><span class="badge badge-secondary">Reports</span></h1>
+
+
+<div class="wrapper">
+   <div class="box1"><table class="table">
+  <thead class="thead">
+    <tr>
+      
+      <th scope="col">Questions</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Question 1</td>
+    </tr>
+    <tr>
+      
+      <td>Question 2</td>
+    </tr>
+    <tr>
+      
+      <td>Question 3</td>
+    </tr>
+  </tbody>
+</table></div>
+   <div class="box2"><table class="table">
+  <thead class="thead">
+    <tr>
+      
+      <th scope="col">Answers</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Answer 1</td>
+    </tr>
+    <tr>
+      
+      <td>Answer 2</td>
+    </tr>
+    <tr>
+      
+      <td>Answer 3</td>
+    </tr>
+  </tbody>
+</table></div>
+   <div class="box3">
+       
+<table class="table">
+  <thead class="thead">
+    <tr>
+      
+      <th scope="col">Comments</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      
+      <td>Comment 1</td>
+    </tr>
+    <tr>
+      
+      <td>Comment 2</td>
+    </tr>
+    <tr>
+      
+      <td>Comment 3</td>
+    </tr>
+  </tbody>
+</table>
+   </div>
+</div>
+
+
+
+    <h1><span class="badge badge-secondary">Users Reported</span></h1>
+
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Username</th>
+      <th scope="col">Name</th>
+      <th scope="col">Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>johny1</td>
+      <td>John</td>
+      <td>32</td>
+    </tr>
+    <tr>
+      <td>mary123</td>
+      <td>Mary</td>
+      <td>-35</td>
+    </tr>
+    <tr>
+      <td>redflag</td>
+      <td>Simon</td>
+      <td>75</td>
+    </tr>
+    <tr>
+      <td>Suzz987</td>
+      <td>Susan</td>
+      <td>3</td>
+    </tr>
+  </tbody>
+</table>
+
+    <h1><span class="badge badge-secondary">Promote</span></h1>
+
+
+<div class="wrapper2">
+   <div class="box1"><table class="table">
+    <thead class="thead">
+    <tr>
+      <th scope="col">Users</th>
+    </tr>
+    <tr>
+      <th scope="col">Username</th>
+      <th scope="col">Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>johny1</td>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>mary123</td>
+      <td>Mary</td>
+    </tr>
+    <tr>
+      <td>redflag</td>
+      <td>Simon</td>
+    </tr>
+    <tr>
+      <td>Suzz987</td>
+      <td>Susan</td>
+    </tr>
+  </tbody>
+</table></div>
+   <div class="box2">
+    <table class="table">
+    <thead class="thead">
+    <tr>
+      <th scope="col">Moderators</th>
+    </tr>
+    <tr>
+      <th scope="col">Username</th>
+      <th scope="col">Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>johny1</td>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>mary123</td>
+      <td>Mary</td>
+    </tr>
+    <tr>
+      <td>redflag</td>
+      <td>Simon</td>
+    </tr>
+    <tr>
+      <td>Suzz987</td>
+      <td>Susan</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+
+
+</div>
+
+
+</body>
+</html>
