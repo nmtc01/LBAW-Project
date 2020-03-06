4fad7d54779b7d8ecc7db92d3388a9293b60ba5a
antoniopedrodantas <pedro.dantascv@gmail.com>
2020-03-06 14:50:27 +0000

    fixed header in question page

--- website/pages/question.php
+++ website/pages/question.php
@@ -22,10 +22,40 @@
         <nav class="navbar navbar-expand-lg navbar-light navbar-default fixed-top">
             <div class="container">
                 <a href="../pages/home.php" class="navbar-brand">Answerly</a>
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn my-2 my-sm-0" type="submit">Search</button>
-                </form>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <button class="btn my-2 my-sm-0" type="submit">Ask
+                                Something</button>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li>
+                            <a class="nav-link" href="#" >
+                                <i class="fas fa-user fa-lg"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="nav-link" href="#" >
+                                <i class="fas fa-bell fa-lg"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+
+
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn my-2 my-sm-0" type="submit" >Search</button>
+                    </form>
+                </div>
             </div>
         </nav>
     </header>
