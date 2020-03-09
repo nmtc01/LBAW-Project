5ab10b83fe1f080f0bf27e2bdf329467696b7975
Nuno <up201706162@fe.up.pt>
2020-03-09 09:24:13 +0000

    Responsive website

--- website/pages/ask.php
+++ website/pages/ask.php
@@ -15,21 +15,21 @@
     </header>
     <div class="wrapper">
         <div class="row">
-            <div class="col-3">
+            <div class="col-sm-3">
                 <img class="mr-3" src="../resources/bob_iger.jpeg" alt="Generic placeholder image">
             </div>
-            <div class="col-9">
+            <div class="col-sm-9">
                 <div class="media-body">
-                    <h1 class="mt-0">Add a question</h1>
+                    <h1 class="mt-0 row">Add a question</h1>
                     <form>
-                        <div class="form-group">
+                        <div class="form-group row">
                             <label for="exampleFormControlTextarea1">Write your question here</label>
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
-                        <a href="#" class="badge badge-dark badge-pill">+</a>
-                        <div id="form-buttons" class="form-group">
-                            <input type="file" class="form-control-file" id="exampleFormControlFile1" accept="image/png, image/jpeg">
-                            <button class="btn btn-primary btn-lg" type="submit">Add</button>
+                        <a href="#" class="badge badge-dark badge-pill row">+</a>
+                        <div id="form-buttons" class="form-group row">
+                            <input type="file" class="form-control-file col-sm-9" id="exampleFormControlFile1" accept="image/png, image/jpeg">
+                            <button class="btn btn-primary btn-lg col-sm-3" type="submit">Add</button>
                         </div>
                     </form>
                 </div>
