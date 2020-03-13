916215b52d1e55abc9478af3c9c2155b01723b57
Nuno <up201706162@fe.up.pt>
2020-03-13 17:27:30 +0000

    changes on question.php

--- website/pages/question.php
+++ website/pages/question.php
@@ -15,7 +15,46 @@
         ?>
     </header>
 
-    <div class="wrapper container">
+    <div class="col-md-5">
+        <?php
+        $type_r = "right";
+        $title_r = "Popular labels";
+        $labels_r = array(
+            0 => "#Medicine",
+            1 => "#Sports",
+            2 => "#Science",
+            3 => "#Astronomy",
+            4 => "#Programming"
+        );
+        $links_r = array(
+            0 => "../index.php",
+            1 => "../index.php",
+            2 => "../index.php",
+            3 => "../index.php",
+            4 => "../index.php"
+        );
+
+        $type_l = "left";
+        $title_l = "Following";
+        $questions_l = array(
+            0 => "Will Erling Braut Haaland be a future winner of the Ballon d’Or?",
+            1 => "Is it possible for Leicester City to defeat Aston Villa in the English Premier League?",
+            2 => "Do you think that Ronaldo is going to play in the next world cup?",
+            3 => "How can I learn C and C++?"
+        );
+        $links_l = array(
+            0 => "../pages/question.php",
+            1 => "../pages/question.php",
+            2 => "../pages/question.php",
+            3 => "../pages/question.php",
+            4 => "../pages/question.php"
+        );
+        draw_side_bar($type_r, $title_r, $labels_r, $links_r);
+        draw_side_bar($type_l, $title_l, $questions_l, $links_l);
+        ?>
+    </div>
+
+    <div class="wrapper container-fluid col-md-5">
 
         <div class="media row">
             <a class="pr-3 col-sm-2" href="profile.php">
@@ -40,13 +79,13 @@
 
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1"></label>
-                    <textarea class="form-control" placeholder="Do you know the answer to this question?"id="exampleFormControlTextarea1" rows="7"></textarea>
+                    <textarea class="form-control" placeholder="Do you know the answer to this question?"id="exampleFormControlTextarea1" rows="4"></textarea>
                     <button class="btn my-2 my-sm-0" type="submit">Answer</button>
                 </div>
 
                 <div class="form-group">
                     <label for="exampleFormControlTextarea1"></label>
-                    <textarea class="form-control" placeholder="Do you want to comment this question?"id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea class="form-control" placeholder="Do you want to comment this question?"id="exampleFormControlTextarea1" rows="2"></textarea>
                     <button class="btn my-2 my-sm-0" type="submit">Comment</button>
                 </div>
 
