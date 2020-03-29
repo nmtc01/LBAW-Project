5b4f97e4fe478758f007d0ff0d7305fa8b7b94c5
Nuno <up201706162@fe.up.pt>
2020-03-29 12:48:30 +0100

    Updated sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,11 +1,11 @@
 -- Table: user
 DROP TABLE IF EXISTS "user" CASCADE;
 CREATE TABLE "user" (
-    user_id         SERIAL          PRIMARY KEY,
+    id              SERIAL          PRIMARY KEY,
     first_name      TEXT            NOT NULL,
     last_name       TEXT            NOT NULL,
     email           TEXT            NOT NULL UNIQUE,
-    description     TEXT,
+    bio             TEXT,
     username        TEXT            NOT NULL UNIQUE,
     password        TEXT            NOT NULL,
     score           INTEGER         NOT NULL DEFAULT 0              
@@ -14,60 +14,48 @@ CREATE TABLE "user" (
 -- Table: label
 DROP TABLE IF EXISTS label CASCADE;
 CREATE TABLE label (
-    label_id        SERIAL          PRIMARY KEY,
+    id              SERIAL          PRIMARY KEY,
     name            TEXT            NOT NULL          
 );
 
 -- Table: notification
 DROP TABLE IF EXISTS notification CASCADE;
 CREATE TABLE notification (
-    notification_id SERIAL          PRIMARY KEY,
+    id              SERIAL          PRIMARY KEY,
     content         TEXT            NOT NULL,
-    date            DATE            DEFAULT 'today' NOT NULL,
+    date            DATE            DEFAULT 'Now' NOT NULL,
     viewed          BOOLEAN         DEFAULT FALSE NOT NULL,
-    user_id         INTEGER         REFERENCES "user" (user_id) NOT NULL
+    user_id         INTEGER         REFERENCES "user" (id) NOT NULL
 );
 
 -- Table: user_management
 DROP TABLE IF EXISTS user_management CASCADE;
 CREATE TABLE user_management (
-    management_id   SERIAL          PRIMARY KEY,
-    state           TEXT            DEFAULT 'active' NOT NULL,
+    id              SERIAL          PRIMARY KEY,
     status          TEXT            DEFAULT 'user' NOT NULL,
-    user_id         INTEGER         REFERENCES "user" (user_id) NOT NULL
-);
-
--- Table: moderator
-DROP TABLE IF EXISTS moderator CASCADE;
-CREATE TABLE moderator (
-    moderator_id     INTEGER         REFERENCES "user" (user_id) NOT NULL UNIQUE
-);
-
--- Table: administrator
-DROP TABLE IF EXISTS administrator CASCADE;
-CREATE TABLE administrator (
-    administrator_id INTEGER         REFERENCES "moderator" (moderator_id) NOT NULL
+    user_id         INTEGER         REFERENCES "user" (id) NOT NULL
 );
 
 -- Table: question
 DROP TABLE IF EXISTS question CASCADE;
 CREATE TABLE question (
-    question_id     SERIAL          PRIMARY KEY,
-    user_id         INTEGER         REFERENCES "user" (user_id) NOT NULL,
+    id              SERIAL          PRIMARY KEY,
+    user_id         INTEGER         REFERENCES "user" (id) NOT NULL,
     title           TEXT            NOT NULL,
     description     TEXT            NOT NULL,
     nr_likes        INTEGER         DEFAULT 0 NOT NULL,
     nr_dislikes     INTEGER         DEFAULT 0 NOT NULL,
-    question_date   DATE            DEFAULT 'today' NOT NULL           
+    question_date   DATE            DEFAULT 'Now' NOT NULL,
+    marked_answer   INTEGER         REFERENCES "answer" (id) DEFAULT 'null'           
 );
 
 -- Table: answer
 DROP TABLE IF EXISTS answer CASCADE;
 CREATE TABLE answer (
-    answer_id        SERIAL          PRIMARY KEY,
-    user_id          INTEGER         REFERENCES "user" (user_id) NOT NULL,
-    question_id      INTEGER         REFERENCES "question" (question_id) NOT NULL,
-    answer_date      DATE            DEFAULT 'today' NOT NULL,
+    id               SERIAL          PRIMARY KEY,
+    user_id          INTEGER         REFERENCES "user" (id) NOT NULL,
+    question_id      INTEGER         REFERENCES "question" (id) NOT NULL,
+    answer_date      DATE            DEFAULT 'Now' NOT NULL,
     content          TEXT            NOT NULL,
     nr_likes         INTEGER         DEFAULT 0 NOT NULL,
     nr_dislikes      INTEGER         DEFAULT 0 NOT NULL
@@ -76,11 +64,11 @@ CREATE TABLE answer (
 -- Table: comment
 DROP TABLE IF EXISTS comment CASCADE;
 CREATE TABLE comment (
-    comment_id       SERIAL          PRIMARY KEY,
-    user_id          INTEGER         REFERENCES "user" (user_id) NOT NULL,
-    question_id      INTEGER         REFERENCES "question" (question_id),
-    answer_id        INTEGER         REFERENCES "answer" (answer_id),
-    comment_date     DATE            DEFAULT 'today' NOT NULL,
+    id               SERIAL          PRIMARY KEY,
+    user_id          INTEGER         REFERENCES "user" (id) NOT NULL,
+    question_id      INTEGER         REFERENCES "question" (id),
+    answer_id        INTEGER         REFERENCES "answer" (id),
+    comment_date     DATE            DEFAULT 'Now' NOT NULL,
     content          TEXT            NOT NULL,
     CHECK (
         (question_id IS NOT NULL AND answer_id IS NULL) OR
@@ -91,12 +79,11 @@ CREATE TABLE comment (
 -- Table: vote
 DROP TABLE IF EXISTS vote CASCADE;
 CREATE TABLE vote (
-    vote_id          SERIAL          PRIMARY KEY,
-    "like"           BOOLEAN         NOT NULL,
-    dislike          BOOLEAN         NOT NULL,
-    user_id          INTEGER         REFERENCES "user" (user_id) NOT NULL,
-    question_id      INTEGER         REFERENCES "question" (question_id),
-    answer_id        INTEGER         REFERENCES "answer" (answer_id),
+    id               SERIAL          PRIMARY KEY,
+    "vote"           BOOLEAN         NOT NULL,
+    user_id          INTEGER         REFERENCES "user" (id) NOT NULL,
+    question_id      INTEGER         REFERENCES "question" (id),
+    answer_id        INTEGER         REFERENCES "answer" (id),
     CHECK (
         (question_id IS NOT NULL AND answer_id IS NULL) OR
         (question_id IS NULL AND answer_id IS NOT NULL)
@@ -106,11 +93,11 @@ CREATE TABLE vote (
 -- Table: report
 DROP TABLE IF EXISTS report CASCADE;
 CREATE TABLE report (
-    report_id        SERIAL          PRIMARY KEY,
-    user_id          INTEGER         REFERENCES "user" (user_id),
-    question_id      INTEGER         REFERENCES "question" (question_id),
-    answer_id        INTEGER         REFERENCES "answer" (answer_id),   
-    comment_id       INTEGER         REFERENCES "comment" (comment_id),
+    id               SERIAL          PRIMARY KEY,
+    user_id          INTEGER         REFERENCES "user" (id),
+    question_id      INTEGER         REFERENCES "question" (id),
+    answer_id        INTEGER         REFERENCES "answer" (id),   
+    comment_id       INTEGER         REFERENCES "comment" (id),
     CHECK(
         (user_id IS NOT NULL AND question_id IS NULL AND answer_id IS NULL AND comment_id IS NULL) OR
         (user_id IS NULL AND question_id IS NOT NULL AND answer_id IS NULL AND comment_id IS NULL) OR
@@ -122,30 +109,27 @@ CREATE TABLE report (
 -- Table: report_status
 DROP TABLE IF EXISTS report_status CASCADE;
 CREATE TABLE report_status (
-    status_id        SERIAL          PRIMARY KEY,
-    report_id        INTEGER         REFERENCES "report" (report_id) NOT NULL,
+    id               SERIAL          PRIMARY KEY,
+    report_id        INTEGER         REFERENCES "report" (id) NOT NULL,
     state            TEXT            DEFAULT 'unresolved' NOT NULL,
     comment          TEXT,
-    responsible_user INTEGER         REFERENCES "moderator" (moderator_id) NOT NULL
-);
-
--- Table: marked_answer
-DROP TABLE IF EXISTS marked_answer CASCADE;
-CREATE TABLE marked_answer (
-    question_id      INTEGER         REFERENCES "question" (question_id) NOT NULL,
-    answer_id        INTEGER         REFERENCES "answer" (answer_id) NOT NULL
+    responsible_user INTEGER         REFERENCES "user_management" (user_id) NOT NULL,
+    CHECK(
+        (responsible_user.status = 'moderator') OR
+        (responsible_user.status = 'administrator')
+    )
 );
 
 -- Table: following
 DROP TABLE IF EXISTS following CASCADE;
 CREATE TABLE following (
-    user_id          INTEGER         REFERENCES "user" (user_id) NOT NULL,
-    label_id         INTEGER         REFERENCES "label" (label_id) NOT NULL
+    user_id          INTEGER         REFERENCES "user" (id) NOT NULL,
+    label_id         INTEGER         REFERENCES "label" (id) NOT NULL
 );
 
 -- Table: about
 DROP TABLE IF EXISTS about CASCADE;
 CREATE TABLE about (
-    question_id      INTEGER         REFERENCES "question" (question_id) NOT NULL,
-    label_id         INTEGER         REFERENCES "label" (label_id) NOT NULL
+    question_id      INTEGER         REFERENCES "question" (id) NOT NULL,
+    label_id         INTEGER         REFERENCES "label" (id) NOT NULL
 );
