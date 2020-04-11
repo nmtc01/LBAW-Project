13d45b2c6b19b0bda718020fe45c8f050b367546
Nuno <up201706162@fe.up.pt>
2020-04-11 16:28:55 +0100

    Finished transactions

--- sql/database.sql
+++ sql/database.sql
@@ -416,29 +416,61 @@ CREATE TRIGGER marked_answer
     EXECUTE PROCEDURE marked_answer();
 
 -----------------------------------------
--- TRANSACTIONS
+-- TRANSACTIONS FUNCTIONS
 -----------------------------------------
---Trigger 01
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
-    INSERT INTO "user" (first_name, last_name, email, bio, username, password)
-        VALUES ('antonio', 'aparicio', 'toutolo@cenas.com', 'nao tenho mts amigos', 'tonicio', 'sha256woendo+2ph«09328');
+--Transaction 1
+CREATE OR REPLACE FUNCTION add_user_management(first_name TEXT, last_name TEXT, email TEXT, bio TEXT, username TEXT, password TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO "user" (first_name, last_name, email, bio, username, password) 
+		VALUES (first_name, last_name, email, bio, username, password);
+	INSERT INTO user_management (user_id)
+		VALUES (currval('user_id_seq'));
+END;
+$$ LANGUAGE plpgsql;
 
-    INSERT INTO user_management (user_id)
-        VALUES (currval('user_id_seq'));
+--Transaction 2
+CREATE OR REPLACE FUNCTION add_report_status(reporter_id INT, user_id INT, description TEXT, responsible_user INT) RETURNS void AS $$
+BEGIN
+	INSERT INTO report (reporter_id, user_id, description)
+		VALUES (reporter_id, user_id, description);
 
-COMMIT;
+	INSERT INTO report_status (report_id, responsible_user)
+		VALUES (currval('report_id_seq'), responsible_user);
+END;
+$$ LANGUAGE plpgsql;
 
---Trigger 02
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
-    INSERT INTO report (user_id, question_id, answer_id, comment_id)
-        VALUES (1, 2, NULL, NULL);
+--Transaction 3
+CREATE OR REPLACE FUNCTION answered_question_notif(answer_user_id INT, question_user_id INT, question_id INT, answer_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO answer (user_id, question_id, content)
+		VALUES (answer_user_id, question_id, answer_content);
 
-    INSERT INTO report_status (report_id, comment, responsible_user)
-        VALUES (currval('report_id_seq'), 'tou tolo', 2);
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id = answer_user_id), ' answered your question!'), question_user_id);
+END;
+$$ LANGUAGE plpgsql;
 
-COMMIT;
+--Transaction 4
+CREATE OR REPLACE FUNCTION commented_question_notif(comment_user_id INT, question_user_id INT, question_id INT, comment_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO comment (user_id, question_id, content)
+		VALUES (comment_user_id, question_user_id, comment_content);
+
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id=comment_user_id), ' commented your question!'), question_user_id);
+END;
+$$ LANGUAGE plpgsql;
+
+--Transaction 5
+CREATE OR REPLACE FUNCTION commented_answer_notif(comment_user_id INT, answer_user_id INT, answer_id INT, comment_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO comment (user_id, answer_id, content)
+		VALUES (comment_user_id, answer_user_id, comment_content);
+
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id=comment_user_id), ' commented your answer!'), answer_user_id);
+END;
+$$ LANGUAGE plpgsql;
 
 -----------------------------------------
 -- end
--- sql/transactions.sql
+++ sql/transactions.sql
@@ -1,89 +1,9 @@
 -----------------------------------------
 -- TRANSACTIONS
 -----------------------------------------
+
 --Transaction 1
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	do $$
-		DECLARE
-			NEXT_USER_ID integer := nextval('user_id_seq');
-	begin
-		INSERT INTO "user" (id, first_name, last_name, email, bio, username, password)
-			VALUES (NEXT_USER_ID, 'joao', 'albino', 'moreira@gmail.com', 'UP student', 'jalbino', '261345b0529b50d25f9d3b0a3276c09380ef5c5824548d04a240413cd32f02e8');
-
-		INSERT INTO user_management (user_id)
-			VALUES (NEXT_USER_ID);
-	end $$;
-COMMIT;
-
---Transaction 2
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-    	do $$
-		DECLARE
-			NEXT_REPORT_ID integer := nextval('report_id_seq');
-	begin
-		INSERT INTO report (id, reporter_id, user_id, description)
-			VALUES (NEXT_REPORT_ID, 1, 27, 'Offensive behaviour');
-
-		INSERT INTO report_status (report_id, responsible_user)
-			VALUES (NEXT_REPORT_ID, 1);
-	end $$;
-COMMIT;
-
---Transaction 3
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	do $$
-		DECLARE
-			USER_RECIEVED_NOTIFICATION_ID integer := (select user_id from question where question.id=1);
-			USER_SEND_NOTIFICATION_USERNAME text := (select username from "user" where id=1);
-			ANSWER_CONTENT text := CONCAT(USER_SEND_NOTIFICATION_USERNAME, ' answered your question!');
-	begin
-		INSERT INTO answer (user_id, question_id, content)
-			VALUES (1, 1,'That is a detail of divine creation!');
-
-		INSERT INTO notification (content, user_id)
-			VALUES (ANSWER_CONTENT, USER_RECIEVED_NOTIFICATION_ID);
-	end $$;
-COMMIT;
-
---Transaction 4
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	do $$
-		DECLARE
-			USER_RECIEVED_NOTIFICATION_ID integer := (select user_id from question where question.id=1);
-			USER_SEND_NOTIFICATION_USERNAME text := (select username from "user" where id=1);
-			COMMENT_CONTENT text := CONCAT(USER_SEND_NOTIFICATION_USERNAME, ' commented your question!');
-	begin
-		INSERT INTO comment (user_id, question_id, content)
-			VALUES (1, 1,'Irrelevant question!');
-
-		INSERT INTO notification (content, user_id)
-			VALUES (COMMENT_CONTENT, USER_RECIEVED_NOTIFICATION_ID);
-	end $$;
-COMMIT;
-
---Transaction 5
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	do $$
-		DECLARE
-			USER_RECIEVED_NOTIFICATION_ID integer := (select user_id from answer where answer.id=1);
-			USER_SEND_NOTIFICATION_USERNAME text := (select username from "user" where id=1);
-			COMMENT_CONTENT text := CONCAT(USER_SEND_NOTIFICATION_USERNAME, ' commented your answer!');
-	begin
-		INSERT INTO comment (user_id, answer_id, content)
-			VALUES (1, 1,'Irrelevant answer!');
-
-		INSERT INTO notification (content, user_id)
-			VALUES (COMMENT_CONTENT, USER_RECIEVED_NOTIFICATION_ID);
-	end $$;
-COMMIT;
-
-
-CREATE OR REPLACE FUNCTION test(first_name TEXT, last_name TEXT, email TEXT, bio TEXT, username TEXT, password TEXT) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION add_user_management(first_name TEXT, last_name TEXT, email TEXT, bio TEXT, username TEXT, password TEXT) RETURNS void AS $$
 BEGIN
 	INSERT INTO "user" (first_name, last_name, email, bio, username, password) 
 		VALUES (first_name, last_name, email, bio, username, password);
@@ -94,5 +14,69 @@ $$ LANGUAGE plpgsql;
 
 BEGIN TRANSACTION;
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-	SELECT test($first_name, $last_name, $email, $bio, $username, $password);
+	SELECT add_user_management($first_name, $last_name, $email, $bio, $username, $password);
+COMMIT;
+
+--Transaction 2
+CREATE OR REPLACE FUNCTION add_report_status(reporter_id INT, user_id INT, description TEXT, responsible_user INT) RETURNS void AS $$
+BEGIN
+	INSERT INTO report (reporter_id, user_id, description)
+		VALUES (reporter_id, user_id, description);
+
+	INSERT INTO report_status (report_id, responsible_user)
+		VALUES (currval('report_id_seq'), responsible_user);
+END;
+$$ LANGUAGE plpgsql;
+
+BEGIN TRANSACTION;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+    SELECT  add_report_status($reporter_id, $user_id, $description, $responsible_user);
+COMMIT;
+
+--Transaction 3
+CREATE OR REPLACE FUNCTION answered_question_notif(answer_user_id INT, question_user_id INT, question_id INT, answer_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO answer (user_id, question_id, content)
+		VALUES (answer_user_id, question_id, answer_content);
+
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id = answer_user_id), ' answered your question!'), question_user_id);
+END;
+$$ LANGUAGE plpgsql;
+
+BEGIN TRANSACTION;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	SELECT answered_question_notif($answer_user_id, $question_user_id, $question_id, $answer_content);
+COMMIT;
+
+--Transaction 4
+CREATE OR REPLACE FUNCTION commented_question_notif(comment_user_id INT, question_user_id INT, question_id INT, comment_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO comment (user_id, question_id, content)
+		VALUES (comment_user_id, question_user_id, comment_content);
+
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id=comment_user_id), ' commented your question!'), question_user_id);
+END;
+$$ LANGUAGE plpgsql;
+
+BEGIN TRANSACTION;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	SELECT commented_question_notif($comment_user_id, $question_user_id, $question_id, $comment_content);
+COMMIT;
+
+--Transaction 5
+CREATE OR REPLACE FUNCTION commented_answer_notif(comment_user_id INT, answer_user_id INT, answer_id INT, comment_content TEXT) RETURNS void AS $$
+BEGIN
+	INSERT INTO comment (user_id, answer_id, content)
+		VALUES (comment_user_id, answer_user_id, comment_content);
+
+	INSERT INTO notification (content, user_id)
+		VALUES (CONCAT((SELECT username AS responsible FROM "user" WHERE id=comment_user_id), ' commented your answer!'), answer_user_id);
+END;
+$$ LANGUAGE plpgsql;
+
+BEGIN TRANSACTION;
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+	SELECT commented_answer_notif($comment_user_id, $answer_user_id, $answer_id, $comment_content);
 COMMIT;
