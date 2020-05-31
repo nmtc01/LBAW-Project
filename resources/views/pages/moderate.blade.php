944c3da6a8bb1f0867b31d3135413974da07bac0
Nuno <up201706162@fe.up.pt>
2020-05-31 17:34:24 +0100

    Minor changes

--- resources/views/pages/moderate.blade.php
+++ resources/views/pages/moderate.blade.php
@@ -15,15 +15,19 @@
       <li class="nav-item">
         <a href="" data-target="#reported-users" data-toggle="tab" class="nav-link">Reported Users</a>
       </li>
+      @if(Auth::user()->getUserCurrentRole() == 'administrator')
       <li class="nav-item">
         <a href="" data-target="#promote" data-toggle="tab" class="nav-link">Promote</a>
       </li>
+      @endif
     </ul>
 
     <div class="tab-content py-4">
             @include('partials.reports')
             @include('partials.reported_users')
+            @if(Auth::user()->getUserCurrentRole() == 'administrator')
             @include('partials.promote')   
+            @endif
     </div>
 
 </div>
